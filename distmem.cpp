77ea2bc6b4b085be6f36c4c297eb7514a3347c12
Usbuild <njuzhangqichao@gmail.com>
2012-09-12 10:52:39 +0800

    add basic datatype

--- distmem.cpp
+++ distmem.cpp
@@ -1,5 +1,14 @@
 #include "distmem.h"
 #include <iostream>
+#include <map>
+using std::cout;
+using std::endl;
+using std::string;
 int main() {
+	DataAtom da;
+	string s = "Hello World";
+	da.set(s.c_str(), s.size());
+	da.append("\0", 1);
+	cout << da.data << endl;
 	return 0;
 }
