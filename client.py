5fb31307d5846fe7e3517d20a6dd686e22d86e25
Qichao <zqc10@software.nju.edu.cn>
2012-09-23 17:10:47 +0800

    rewrite some client script

--- client.py
+++ client.py
@@ -6,14 +6,18 @@ sock.connect(('localhost', 4327))
 while True:
     s = raw_input("Prompt:>");
     if s == "exit": break;
-    if len(s.split())< 3 :
-        s += " rubbish";
     s = s.lstrip()
     sa = s.split()
     p = []
     p.append(sa[0])
-    p.append(sa[1])
-    p.append(s[s.find(" " + p[-1] +" ") + len(p[-1]) + 1:].lstrip())
+    if sa[0] == 'set':
+        p.append(sa[1])
+        p.append(s[s.find(" " + p[-1] +" ") + len(p[-1]) + 1:].lstrip())
+    elif sa[0] == 'get':
+        p.append(sa[1])
+    else:
+        print 'Unknown Command'
+        continue
     request = "*" + str(len(p)) + "\r\n"
     for param in p:
         request += "$" + str(len(param)) +"\r\n"
